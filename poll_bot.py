3ab73bb4fb217674b5ecf5cb1a322908c0a6296b
N-010 <chelsig96@gmail.com>
2022-07-01 21:53:21 +0300

    Cleanup

--- poll_bot.py
+++ poll_bot.py
@@ -12,13 +12,6 @@ async def main():
     token = os.environ.get("BOT_ACCESS_TOKEN")
     print(F"Token: {token}\n")
 
-    # print(kangaroo_twelve(b"test"))
-
-    # print(str_signature_to_bytes("oamjjcmpbgfmjlniknkmejhacaphflcchbfiaelgbdndmigbkinngdjplcjihlacdfncbabnihanfhpjnmngkfgbedhdclfhfhckkdmdahnblmgcebcffjipdainadaa"))
-
-    print(verify(get_public_key_from_id("BPHPEIHDBADJJPMHBEEJLIGBFFCAONGDCOEKIPHPPHCIJDAECDOIGIPFKGGDAKDMADNMKO"), kangaroo_twelve(b"test"), str_signature_to_bytes(
-        "oamjjcmpbgfmjlniknkmejhacaphflcchbfiaelgbdndmigbkinngdjplcjihlacdfncbabnihanfhpjnmngkfgbedhdclfhfhckkdmdahnblmgcebcffjipdainadaa")))
-
 
 if __name__ == "__main__":
     asyncio.run(main())
