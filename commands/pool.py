4a21ea80121058d8fae3ee0821328a3e9c268429
N-010 <chelsig96@gmail.com>
2022-07-02 18:57:53 +0300

    Pool: register

--- commands/pool.py
+++ commands/pool.py
@@ -1,5 +1,6 @@
 
 from asyncio import Queue, Task, create_task
+import asyncio
 from inspect import iscoroutinefunction
 from typing import Optional
 
@@ -7,36 +8,43 @@ from typing import Optional
 class PoolCommands():
     def __init__(self) -> None:
         self.pool = Queue()
-        self.task: Optional[Task] = None
+        self._task: Optional[Task] = None
 
     async def add_command(self, func, *args, **kwargs):
+
         if self.pool.full():
             await self.pool.put((func, args, kwargs))
         else:
             self.pool.put_nowait((func, args, kwargs))
 
-    async def _execute(self):
+    async def execute(self):
         while True:
             try:
                 func_tuple = await self.pool.get()
                 func = func_tuple[0]
                 args = func_tuple[1]
                 kwargs = func_tuple[2]
+
                 if iscoroutinefunction(func):
                     await func(*args, **kwargs)
                 else:
                     func(*args, **kwargs)
+
             finally:
-                self.pool.task_done()
+                try:
+                    self.pool.task_done()
+                except ValueError:
+                    pass
 
     def __len__(self):
         return self.pool.qsize()
 
     def start(self):
-        self.task = create_task(self.execute())
+        loop = asyncio.get_event_loop()
+        self._task = loop.create_task(self.execute())
 
     async def stop(self):
-        if len(self) > 0:
-            await self.pool.join()
-
-        self.task.cancel()
+        await self.pool.join()
+        
+        if self._task != None:
+            self._task.cancel()
