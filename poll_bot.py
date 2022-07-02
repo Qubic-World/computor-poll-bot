56be1e6146a46a31f2475a0ce100512e8d0cd650
N-010 <chelsig96@gmail.com>
2022-07-02 13:41:52 +0300

    Registry command

--- poll_bot.py
+++ poll_bot.py
@@ -1,17 +1,48 @@
-import asyncio
+import logging
 import os
 
+from discord.ext import commands
 from dotenv import load_dotenv
-from algorithms.verify import get_public_key_from_id, verify, str_signature_to_bytes, kangaroo_twelve
+
+from verify.message import get_username_id_from_message, is_valid_message
+from verify.user import is_existing_user
+
+poll_bot = commands.Bot(command_prefix="/")
 
 
-async def main():
+@poll_bot.command()
+async def register(ctx, *, json):
+    result = is_valid_message(json)
+    if result[0] == False:
+        await ctx.reply(result[1])
+        return
+
+    username_id = get_username_id_from_message(json)
+
+    username_result = await is_existing_user(poll_bot, username_id)
+    if username_result[0] == False:
+        await ctx.reply("No such user could be found")
+        return
+
+    await ctx.reply(f"User {username_result[1]} added")
+
+
+@poll_bot.event
+async def on_ready():
+    print("On ready")
+
+
+def main():
     # Read from .env
     load_dotenv()
 
     token = os.environ.get("BOT_ACCESS_TOKEN")
-    print(F"Token: {token}\n")
+
+    try:
+        poll_bot.run(token)
+    except Exception as e:
+        logging.error(e)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
